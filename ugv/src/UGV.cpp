e56adffd894bacabf5ce2f2c534388ef270d515d
Johan Barradas <jbarradassubmssmu@gmail.com>
2020-05-14 20:42:09 -0500

    Add files via upload
    src folder

---
+++ ugv/src/UGV.cpp
@@ -0,0 +1,105 @@
+/*
+Chris Collander, cmcollander@gmail.com, December 2018
+
+This class extends hardware_interface:Robot_HW handle communication between  to a Roboteq motor controller. 
+This code should be called by UGVinterface.cpp
+*/
+
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+//debugging purposes
+#include <iostream>
+
+#include "ugv/RoboteqDevice.h"
+#include "ugv/ErrorCodes.h"
+#include "ugv/Constants.h"
+#include <unistd.h>
+
+class UGV : public hardware_interface::RobotHW {
+private:
+	RoboteqDevice device;
+	hardware_interface::JointStateInterface jnt_state;
+	hardware_interface::VelocityJointInterface jnt_vel;
+	
+	double cmd[2];
+	double pos[2];
+	double vel[2];
+	double eff[2];
+	
+	double vel_in_value;
+	double vel_out_value;
+	double odom_value;
+		
+	int ret, initial_encoder[2];
+public:
+	UGV(std::string mcusb, double vel_in_scale, double vel_out_scale, double odom_scale) {
+		// Pull in arguments
+		vel_in_value = vel_in_scale;
+		vel_out_value = vel_out_scale;
+		
+		odom_value = odom_scale;
+		
+		// Initialize all values to zero
+		cmd[0] = 0; cmd[1] = 0;
+		pos[0] = 0; pos[1] = 0;
+		vel[0] = 0; vel[1] = 0;
+		eff[0] = 0; eff[1] = 0;
+
+		// Create our joints and register them
+		// TODO: Why did I set these to these indices???? They work in my case though...
+		hardware_interface::JointStateHandle stateA("Right",&pos[1],&vel[0],&eff[0]);
+		hardware_interface::JointStateHandle stateB("Left",&pos[0],&vel[1],&eff[1]);
+		jnt_state.registerHandle(stateA);
+		jnt_state.registerHandle(stateB);
+		registerInterface(&jnt_state);
+		hardware_interface::JointHandle velA(jnt_state.getHandle("Right"),&cmd[1]);
+		hardware_interface::JointHandle velB(jnt_state.getHandle("Left"),&cmd[0]);
+		jnt_vel.registerHandle(velA);
+		jnt_vel.registerHandle(velB);
+		registerInterface(&jnt_vel);
+		
+		// Prepare our motor controller
+		int status = device.Connect(mcusb);
+		device.GetValue(_ABCNTR,1,initial_encoder[0]);
+		device.GetValue(_ABCNTR,2,initial_encoder[1]);
+	}
+
+	// Functions to assist with ROS time-management
+	ros::Time getTime() const {return ros::Time::now();}
+        ros::Duration getPeriod() const {return ros::Duration(0.05);}
+
+	void read() {
+		// Determine pos, vel, and eff and place them into variables
+		
+		// _ABSPEED returns RPM, we need Rads/Sec
+		vel[0] = device.GetValue(_ABSPEED,1,ret);
+		vel[0] = (ret+8.0)*vel_in_value; //add 8 to match the current RPM we are getting
+		//std::cerr << "wheel 0= "<< ret << "r/s: " << vel[0] << std::endl;
+		vel[1] = device.GetValue(_ABSPEED,2,ret);
+		vel[1] = ret*vel_in_value;
+		//std::cerr << "wheel 1= "<< ret << "r/s: " << vel[1] << std::endl;
+		
+		pos[0] = device.GetValue(_ABCNTR,1,ret);
+		pos[0] = (ret-initial_encoder[0])*odom_value;
+                //std::cerr << pos[0] <<std::endl;
+
+		pos[1] = device.GetValue(_ABCNTR,2,ret);
+		pos[1] = (ret-initial_encoder[1])*odom_value;
+		//std::cerr << pos[1] <<std::endl;
+		
+		eff[0] = device.GetValue(_MOTPWR,1,ret);
+		eff[0] = ret;
+		eff[1] = device.GetValue(_MOTPWR,2,ret);
+		eff[1] = ret;
+	}
+
+	void write() {
+		// Write 'cmd' out to the motor driver
+		// cmd[] is in rads per second
+		// _GO takes in a value from -1000 to 1000 linearly from -60RPM to 60RPM
+		device.SetCommand(_GO,1,(int)(cmd[0]*vel_out_value));
+		
+		device.SetCommand(_GO,2,(int)(cmd[1]*vel_out_value));
+	}
+};
---
+++ ugv/src/UGVinterface.cpp
@@ -0,0 +1,83 @@
+/*
+Chris Collander, cmcollander@gmail.com, December 2018
+
+This ROS node is used with UGV.cpp for communication between /cmd_vel and /odom to a Roboteq motor controller.
+
+ROS Parameters
+	/motor_controller_usb, which Linux device the Roboteq controller is registered as. Default: /dev/ttyACM0
+        /ugv_vel_in_scalar, Default: (PI/3.0)
+        /ugv_vel_out_scalar, Default: (15.9155*0.33333333)
+        /ugv_odom_scalar, Default: (PI/(6000*4.20))
+
+Please ensure your controller has the following settings:
+* Left wheel connected to motor 1
+* Right wheel connected to motor 2
+* Encoders connected, tested, and set to tuned PID coefficients in closed-loop speed control
+* Encoder ticks per revolution properly calculated through gear ratio and encoder specs
+* Max velocity for each wheel set to 60 RPM
+* Some parameters may need to be tuned and changed based on your build. Trial and error.
+TODO: At a later date, I will add the configuration file for my motor driver (MDC2460)
+
+After wheels respond to correct direction, speed can be adjusted through proper calculation (or trial and error)
+After wheels respond to correct speed, odom can be adjusted through proper calculation (or trial and error)
+
+My current method for tuning odom:
+* Visualize in Rviz with /odom as global frame.
+* View lidar and transform tree (tf)
+* Rotate robot clockwise.
+* * If lidar scan rotates to the right: Decrease ODOM_NUM and/or Increase ODOM_DEN
+* * If lidar scan rotates to the left:  Increase ODOM_NUM and/or Decrease ODOM_DEN
+* When robot is able to rotate N-rotations without rotation of the laser scan, odom is properly tuned (my N=100)
+*/
+
+#include "UGV.cpp"
+#include <ros/ros.h>
+#include <controller_manager/controller_manager.h>
+
+#define PI 3.14159265359
+
+// Default input/output scalar values
+#define USB_DEFAULT "/dev/ttyACM0"
+#define VEL_IN_DEFAULT (2*PI/(60.0))//conversion factor//PI/3.0)
+#define VEL_OUT_DEFAULT (60.0/(2*PI))    //10.1307)//14978)
+//define VEL_OUT_DEFAULT (15.9155*0.33333333)
+//#define VEL_OUT_DEFAULT (8.46092)//131.7/4.1)//32.925)
+#define ODOM_DEFAULT (PI/368.5)//(85*4.20))
+
+int main(int argc, char** argv) {
+        ros::init(argc, argv, "UGV");
+        ros::NodeHandle nh;
+        
+        // Parameter Variables
+        std::string MotorControllerUSB;
+        double velInScalar;
+        double velOutScalar;
+        double odomScalar;
+        
+	// Obtain ROS parameters
+        nh.param<std::string>("/motor_controller_usb", MotorControllerUSB, USB_DEFAULT);
+        nh.param<double>("/ugv_vel_in_scalar", velInScalar, VEL_IN_DEFAULT);
+        nh.param<double>("/ugv_vel_out_scalar", velOutScalar, VEL_OUT_DEFAULT);
+        nh.param<double>("/ugv_odom_scalar", odomScalar, ODOM_DEFAULT);
+        // Create our hardware_interface and connect it to the controller manager
+
+        UGV ugv(MotorControllerUSB,velInScalar,velOutScalar,odomScalar);
+        controller_manager::ControllerManager cm(&ugv,nh);
+
+	// ROS time handling
+        ROS_WARN_STREAM("period: " << ugv.getPeriod().toSec());
+        ros::Rate rate(1.0 / ugv.getPeriod().toSec());
+        ros::AsyncSpinner spinner(1);
+        spinner.start();
+
+	// Infinite loop for control calculation
+        while(ros::ok()) {
+                ugv.read();
+                cm.update(ugv.getTime(), ugv.getPeriod());
+                ugv.write();
+                rate.sleep();
+        }
+        spinner.stop();
+        return 0;
+}
+
